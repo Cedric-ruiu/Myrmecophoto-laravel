d61539c7d0c7a4880aa64f382ea8d35ada6280f8
Cedric-ruiu <cedric.ruiu@gmail.com>
2019-02-14 21:38:23 +0100

    feat(build): init builder
    dev with
    - autoprefixer
    - css-mqpacker
    prod with
    - cssnano
    - version

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,30 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.options({
+    postCss: [
+        require("autoprefixer")({
+            grid: "autoplace"
+        }),
+        require("css-mqpacker")({
+            sort: true
+        })
+    ],
+    hmrOptions: {
+        host: 'myrmecophoto.test',
+        port: 8080
+    }
+});
+
+if (mix.inProduction()) {
+    mix.version();
+
+    mix.options({
+        postCss: [
+            require("cssnano")({
+                grid: "autoplace"
+            })
+        ]
+    });
+}
